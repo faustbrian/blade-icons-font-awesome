5dc9aa10d0affa56e7b06eeb746185880888fedb
Brian Faust <hello@basecode.sh>
2023-04-26 04:01:36 +0300

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ src/ServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BombenProdukt\BladeIcons\FontAwesome;
+
+use BombenProdukt\BladeIcons\Facades\IconFamilyRegistry;
+use BombenProdukt\BladeIcons\IconFamily;
+use BombenProdukt\PackagePowerPack\Package\AbstractServiceProvider;
+
+final class ServiceProvider extends AbstractServiceProvider
+{
+    public function packageRegistered(): void
+    {
+        IconFamilyRegistry::push(IconFamily::fromDirectory('font-awesome', __DIR__ . '/../resources/svg'));
+    }
+}
